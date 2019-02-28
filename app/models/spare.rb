26040e6419a4a536d461b3877cdcd935d1cf5c90
Ethan Sonza <ethanjericho@yahoo.com>
2019-03-01 00:46:34 +1000

    date format

--- app/models/spare.rb
+++ app/models/spare.rb
@@ -1,3 +1,11 @@
 class Spare < ApplicationRecord
   scope :search, -> (query) { where("description LIKE ? OR date_delivered LIKE ? OR date_warranty LIKE ? OR stock LIKE ? OR balance LIKE ?",  "%#{query}%","%#{query}%","%#{query}%","%#{query}%","%#{query}%") }
+
+  def delivered
+    self.date_delivered.strftime("%b %d %Y")
+  end
+  
+  def warranty
+    self.date_warranty.strftime("%b %d %Y")
+  end
 end
