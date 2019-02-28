e1dbc780a571175a0ebeb9f8c62fd24dcf94eb0b
Ethan Sonza <ethanjericho@yahoo.com>
2019-03-01 03:16:31 +1000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "Creating a user"
+
+user = User.new
+user.email = "test@test.com"
+user.password = "password"
+user.first_name = "Jesus"
+user.last_name = "Hordejan"
+user.save!
+
+p "Creating test servers"
+
+3.times do |i|
+  server = Server.new
+  server.host_name = "192.168.1.#{i+1}"
+  server.model = "Model #{i+1}"
+  server.serial_no = "#{rand(1000..5000)} #{rand(1000..5000)} #{rand(10..99)}"
+  server.save!
+end
+
+p "Creating test spares"
+
+3.times do |i|
+  spare = Spare.new
+  spare.description = "Description #{i+1}"
+  spare.date_delivered = Date.today - i
+  spare.date_warranty = Date.today + (i+1).month
+  spare.stock = rand(1..100)
+  spare.balance = rand(150.00..2000.00).round(2)
+  spare.save!
+end
